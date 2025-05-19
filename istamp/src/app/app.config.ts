40fd06df6ade5791ee0a6a0df9d445802745740c
TongPPTH <phongpat003@gmail.com>
2025-05-19 15:01:06 +0700

    frist commit

--- istamp/src/app/app.config.ts
+++ istamp/src/app/app.config.ts
@@ -1,8 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
-import { routes } from './app.routes';
+const firebaseConfig = {
+  apiKey: 'AIzaSyCQFqfgcwI6ynvVpXfLOAOYEHstCegL9CQ',
+  authDomain: 'image-cropper-control.firebaseapp.com',
+  projectId: 'image-cropper-control',
+  storageBucket: 'image-cropper-control.appspot.com',
+  messagingSenderId: '398885936902',
+  appId: '1:398885936902:web:f09e8854362657790e4887',
+};
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideAnimations(),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideStorage(() => getStorage()),
+  ],
 };
