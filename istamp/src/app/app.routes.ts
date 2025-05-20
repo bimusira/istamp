60b4c8de25047458722431dd15851ba149ee8f9d
TongPPTH <phongpat003@gmail.com>
2025-05-20 15:16:11 +0700

    tailwind and v1router

--- istamp/src/app/app.routes.ts
+++ istamp/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { StaffComponent } from './staff/staff.component';
+import { CustomerComponent } from './customer/customer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'staff',
+        component: StaffComponent
+    },
+    {
+        path: 'customer',
+        component: CustomerComponent
+    }
+];
