4a41c7400ba33041c3b15210b0a017247e9cd521
TongPPTH <phongpat003@gmail.com>
2025-06-05 11:39:38 +0700

    error {}

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -32,15 +32,14 @@ import { HttpClient } from '@angular/common/http';
   ],
 })
 export class AppComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router , private http: HttpClient) {}
   
    goTo(path:string) {
     this.router.navigate([`/${path}`]);
-
+   }
   // height = signal(250);
  // width = signal(250);
 
-  constructor(private http: HttpClient) { }
 
   ngOnInit() {
     if (!sessionStorage.getItem('customer_id')) {
@@ -60,5 +59,4 @@ export class AppComponent {
   //imageReady(imageUrl: string) {
    // console.log('Firebase Uploaded Image: ', imageUrl);}
 
-  
 }
