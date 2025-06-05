cb85e183c0ac88b028e2a69bd10f7961a79afc57
bimusira <154392657+bimusira@users.noreply.github.com>
2025-06-05 13:43:24 +0700

    add choose amount page

--- frontend/src/app/customer/customer.component.ts
+++ frontend/src/app/customer/customer.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router'; 
+import { RouterModule, Router } from '@angular/router'; 
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-customer',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './customer.component.html',
   styleUrl: './customer.component.css'
 })
 export class CustomerComponent {
+  constructor(private router: Router) {}
 
+  selectAmount(amount: number) {
+    sessionStorage.setItem('amount', String(amount));
+    const uuid = sessionStorage.getItem('customer_id');
+    this.router.navigate(['/customer/select-temp']);
+  }
 }
