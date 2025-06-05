8c50ae36f76f877330f04ff066c03d0aef4e8c59
ppap26 <65160031@go.buu.ac.th>
2025-06-05 21:27:01 +0700

    mongodb

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GcsService } from './gcs/gcs.service';
 import { GcsController } from './gcs/gcs.controller';
-
+import { UserController } from './user/user.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, GcsController],
+  imports: [AuthModule],
+  controllers: [AppController, GcsController, UserController],
   providers: [AppService, GcsService],
 })
 export class AppModule {}
